a277f35c53272bb4e04aba918e7a4b6a35309145
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-02-02 14:15:27 +0100

    uajndi

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const mainarray=[];
-mainarray.push(document.getElementById("div1"));
-mainarray.push(document.getElementById("div2"));
-mainarray.push(document.getElementById("div3"));
+
 
 
 
 function myfunction(a){
-    mainarray.forEach(i => {
-        i.style.display ="none";
-    });
+    document.getElementById("div1").style.display ="none";
+    document.getElementById("div2").style.display ="none";
+    document.getElementById("div3").style.display ="none";
+
 
     document.getElementById("div"+a).style.display = "inline-block";
 
