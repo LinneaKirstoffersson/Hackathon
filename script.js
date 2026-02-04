4d1374d20a4413bdb6d205189bd1f4c00108cd1b
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-02-04 10:56:41 +0100

    abjk

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function myfunction(a){
     document.getElementById("div2").style.display ="none";
     document.getElementById("div3").style.display ="none";
 
-    document.getElementById("div"+a).style.display = "inline-block";
+    document.getElementById("div"+a).style.display = "block";
 }
