6f1bd05921e065aebf068e93940e720e7825fac9
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-02-04 15:07:03 +0100

    förfärlig js kod

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-for (let i = 0; i < sessionStorage.length; i++) {
-    let a = document.createElement("tr");
-    a.innerHTML = sessionStorage.getItem("assignment");
-    document.getElementById("tblassignments").appendChild(a);
-}
+var assignmentamount;
 
+if (sessionStorage.length <2) {
+    assignmentamount = 0;
+
+} else{
+    assignmentamount = sessionStorage.getItem("assignamount");
+}
 
 function changeWindow(a){
     document.getElementById("div1").style.display ="none";
@@ -16,70 +18,30 @@ function changeWindow(a){
 
 //Assignments
 
+for (let i = 0; i < sessionStorage.length; i++) {
+    let a = document.createElement("tr");
+    a.getAttribute("class", "assignment");
+    a.innerHTML = sessionStorage.getItem("id"+assignmentamount);
+    document.getElementById("tblassignments").appendChild(a);
+}
+
 function createAssignmentForm(){
-    let f = document.createElement("form");
-    f.setAttribute("class", "frmAssignment");
-    f.setAttribute("id", "formthing")
-    f.onsubmit = function yeah(){
-        createAssignment();
-    }
-
-    let n = document.createElement("input");
-    n.setAttribute("type", "text")
-    n.setAttribute("name", "name");
-    f.appendChild(n);
-
-    let d = document.createElement("input");
-    d.setAttribute("type", "date");
-    d.setAttribute("name", "date");
-    f.appendChild(d);
-
-    for (let i = 1; i < 4; i++) {
-        let b = document.createElement("label");
-        b.innerHTML = "hi"
-
-        let a = document.createElement("input");
-        a.setAttribute("type", "radio");
-        a.setAttribute("name", "importance");
-        b.appendChild(a);
-        f.appendChild(b);
-    }
-    let b = document.createElement("input");
-    b.setAttribute("type", "submit");
-    f.appendChild(b);
-
-    document.getElementById("div3").appendChild(f);
+    let f = document.getElementById("assignmentFormTemp");
+    document.getElementById("div3").appendChild(f.content);
 }
 
 function createAssignment(){
+    assignmentamount++;
+    let a = document.getElementById("assignmentTemp");
+    let clon = a.content.cloneNode(true);
+    a.innerHTML.id = "id"+ assignmentamount;
+    document.getElementById("tblassignments").appendChild(clon);
+    sessionStorage.setItem(a.innerHTML.id, a.innerHTML);
+    sessionStorage.setItem("assignamount", assignmentamount)
+}
 
-    let tr = document.createElement("tr");
-
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-
-    let td1lbl = document.createElement("p");
-    td1lbl.innerHTML = "name"
-    td1.appendChild(td1lbl);
-
-    let td2lbl = document.createElement("p");
-    td2lbl.innerHTML = bob[1];
-    td2.appendChild(td2lbl);
-
-    let td3lbl = document.createElement("p");
-    td3lbl.innerHTML = bob[2];
-    td3.appendChild(td3lbl);
-
-    let td4btn = document.createElement("button");
-    td4.appendChild(td4btn);
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-
-    document.getElementById("tblassignments").appendChild(tr);
-    sessionStorage.setItem("assignment", tr.outerHTML);
+function removeAssignment(){
+    sessionStorage.removeItem(event.srcElement.parentNode.parentNode.id);
+    event.srcElement.parentNode.parentNode.remove();
+    
 }
