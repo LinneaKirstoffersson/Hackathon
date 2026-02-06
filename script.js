fdd0b1e0770a606b2ff0bc7eaae571a96d7c6f4b
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-02-06 11:14:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,16 +26,24 @@ for (let i = 0; i < assignmentsArray.length; i++) {
 }
 
 function createAssignment(){
-    let a = document.createElement("tr");
-    a.innerHTML ="<td>"+ document.getElementById("assname").value+"</td><td>"+ document.getElementById("assdate").value+"</td><td>importance</td><td><button onclick='removeAssignment()'>delete assignment</button></td>";
-    a.getAttribute("class", "assignmenttablerow");
-    assignmentsArray.push(a.innerHTML);
-    localStorage.setItem("assignmentsArray", JSON.stringify(assignmentsArray));
-    document.getElementById("assignmenttable").appendChild(a);
-    document.getElementById("assname").value = "";
-    document.getElementById("assdate").value = "";
-    document.getElementById("assignmentform").style.display ="none";
-    document.getElementById("assignmentbtn").style.display ='inline';
+    const assname = document.getElementById("assname");
+    const assdate = document.getElementById("assdate");
+    const assimp = document.getElementById("assimp");
+    if (assname == ""||assdate.value == "") {
+        document.getElementById("asserr").style.display = "inline";
+    } else{
+        let a = document.createElement("tr");
+        a.innerHTML ="<td>"+ assname.value+"</td><td>"+ assdate.value+"</td><td>"+assimp.value+"</td><td><button onclick='removeAssignment()'>Delete assignment</button></td>";
+        a.getAttribute("class", "assignmenttablerow");
+        assignmentsArray.push(a.innerHTML);
+        localStorage.setItem("assignmentsArray", JSON.stringify(assignmentsArray));
+        document.getElementById("assignmenttable").appendChild(a);
+        assname.value = "";
+        document.getElementById("assdate").value = "";
+        document.getElementById("assignmentform").style.display ="none";
+        document.getElementById("assignmentbtn").style.display ='inline';
+        document.getElementById("asserr").style.display = "none";
+    }
 }
 function removeAssignment(){
     assignmentsArray.splice(event.srcElement.parentNode.parentNode.id.substr(-1),1);
